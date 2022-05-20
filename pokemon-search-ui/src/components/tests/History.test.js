dabbc9f07b8441b8ca91f6f18a19bcdc8582d91c
narocroc123 <fergus.corcoran95@gmail.com>
2022-05-20 14:38:59 +0100

    component tests

---
+++ pokemon-search-ui/src/components/tests/History.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import History from "../Search";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "../../reducers/index";
+
+const preLoadedState = {
+  search: {
+    data: {
+      name: "charizard",
+      description:
+        "Spits fire that is hot enough to melt boulders. Known to cause forest fires unintentionally.",
+      is_legendary: false,
+    },
+  },
+  history: ["pikachu", "azelf", "charizard"],
+};
+
+const mockStore = createStore(rootReducer, preLoadedState);
+
+describe("App", () => {
+  test("renders App component", () => {
+    render(
+      <Provider store={mockStore}>
+        <History />
+      </Provider>
+    );
+  });
+});
